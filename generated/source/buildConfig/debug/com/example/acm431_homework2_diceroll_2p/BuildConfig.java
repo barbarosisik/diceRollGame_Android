2fcf82ad7d3f36cc394fc2439639b65c4ab21e0c
Barbaros IŞIK <57237644+barbarosisik@users.noreply.github.com>
2021-02-26 23:18:56 +0300

    Add files via upload

---
+++ generated/source/buildConfig/debug/com/example/acm431_homework2_diceroll_2p/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.acm431_homework2_diceroll_2p;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.acm431_homework2_diceroll_2p";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
